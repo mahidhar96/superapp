1d612276ebb3f427f780e918a16d480785adfe8b
mahidhar96 <mahidhar96@gmail.com>
2021-03-04 14:35:37 +0530

    webappactivity update

--- app/src/main/java/com/mahidhar/superapp/ui/activity/WebAppActivity.kt
+++ app/src/main/java/com/mahidhar/superapp/ui/activity/WebAppActivity.kt
@@ -1,14 +1,19 @@
 package com.mahidhar.superapp.ui.activity
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.GeolocationPermissions
+import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.mahidhar.superapp.R
 
 class WebAppActivity : AppCompatActivity() {
     var source:String? =null
     var name:String?=null
+    private lateinit var webApp:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_app)
@@ -17,7 +22,51 @@ class WebAppActivity : AppCompatActivity() {
         Log.e("name",name.toString())
         Log.e("source",source.toString())
         setTitle(name)
-        val web_app = findViewById<WebView>(R.id.webapp_webview)
-        web_app.loadUrl(source!!)
+        val webApp = findViewById<WebView>(R.id.webapp_webview)
+        webApp.webViewClient = object : WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+//                binding.mainRefreshLayout.isRefreshing = true
+            }
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+//                binding.mainRefreshLayout.isRefreshing = false
+            }
+        }
+
+        webApp.requestFocus()
+        webApp.getSettings().lightTouchEnabled = true
+        webApp.settings.javaScriptEnabled = true
+        webApp.settings.domStorageEnabled = true
+        webApp.settings.useWideViewPort = true
+        webApp.settings.loadWithOverviewMode = true
+        webApp.settings.setGeolocationEnabled(true)
+        webApp.webChromeClient= object:WebChromeClient(){
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                if (newProgress<100){
+
+                }
+            }
+
+            override fun onGeolocationPermissionsShowPrompt(
+                origin: String?,
+                callback: GeolocationPermissions.Callback
+            ) {
+                callback.invoke(origin, true, false)
+            }
+        }
+        webApp.loadUrl(source!!)
+
+    }
+
+    override fun onBackPressed() {
+        return if(this::webApp.isInitialized && webApp.canGoBack()) {
+            webApp.goBack()
+            webApp.canGoBack()
+            Unit
+        } else
+            super.onBackPressed()
+
     }
 }
